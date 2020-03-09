606794c875c870b3ed790913f32710fa2e90294d
Anil9757 <56797977+Anil9757@users.noreply.github.com>
2020-03-09 18:08:50 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,98 +1,16 @@
 <template>
-<div id="app">
-  <v-app id="inspire">
-    <v-content>
-      <v-container
-        fluid
-        fill-height
-      >
-        <v-layout
-          align-center
-          justify-center
-        >
-          <v-flex
-            xs12
-            sm8
-            md4
-          >
-            <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>Login form</v-toolbar-title>
-                <v-spacer></v-spacer>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Login"
-                    name="login"
-                    type="text"
-                    v-model="input.username"
-                  ></v-text-field>
-
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    type="password"
-                    v-model="input.password"
-                  ></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" v-on:click="login">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
-</div>
+  <div class="login">
+    <HelloWorld/>
+  </div>
 </template>
 
 <script>
+import Login from '@/components/HelloWorld.vue'
 export default {
-  name: 'Login',
-  data () {
-    return {
-      input: {
-        username: '',
-        password: ''
-      }
-    }
-  },
-  methods: {
-    login () {
-      // eslint-disable-next-line eqeqeq
-      if (this.input.username != '' && this.input.password != '') {
-        // eslint-disable-next-line eqeqeq
-        if (this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-          this.$emit('authenticated', true)
-          this.$router.replace({ name: 'secure' })
-        } else {
-          console.log('The username and / or password is incorrect')
-        }
-      } else {
-        console.log('A username and password must be present')
-      }
-    }
+  name: 'HelloWorld',
+  components : {
+    Login
   }
+  
 }
-
 </script>
-
-<style scoped>
-    #login {
-        width: 500px;
-        border: 1px solid #CCCCCC;
-        background-color: #FFFFFF;
-        margin: auto;
-        margin-top: 200px;
-        padding: 20px;
-    }
-</style>
